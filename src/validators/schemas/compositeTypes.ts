ac4184741a4afd380a13b18700620d639cc7bd2a
JasonPanosso <jasonpanosso@protonmail.com>
2023-10-13 15:07:21 -0400

    transform composite type fields via zod

--- src/validators/schemas/compositeTypes.ts
+++ src/validators/schemas/compositeTypes.ts
@@ -2,17 +2,25 @@ import { z } from 'zod';
 
 export type CompositeTypeQuery = z.infer<typeof compositeTypeQueryValidator>;
 
+const fieldValidator = z.object({
+  name: z.string(),
+  type: z.string(),
+  isArray: z.boolean(),
+  dimensions: z.number().int(),
+  typeCategory: z.string(),
+});
+
 export const compositeTypeQueryValidator = z.object({
   name: z.string(),
   oid: z.number().int(),
   parentOid: z.number().int(),
-  fields: z.array(
-    z.object({
-      name: z.string(),
-      type: z.string(),
-      isArray: z.boolean(),
-      dimensions: z.number().int(),
-      typeCategory: z.string(),
-    })
+  fields: z.array(fieldValidator).transform((fields) =>
+    fields.reduce(
+      (acc: Record<string, z.infer<typeof fieldValidator>>, cur) => {
+        acc[cur.name] = cur;
+        return acc;
+      },
+      {}
+    )
   ),
 });
